997547731912e1fcb534c373a90ca30d224f2680
amy-1989 <amy1989moran@gmail.com>
2024-04-23 20:15:32 +0000

    added product models and products to products app

--- products/models.py
+++ products/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=250, null=True, blank=True)
+    name = models.CharField(max_length=250)
+    description = models.TextField()
+    image = models.ImageField(null=True, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.ForeignKey('Rating', null=True, blank=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.name
+
+
+class Rating(models.Model):
+    rating = models.CharField(max_length=100, null=True)
+
+
