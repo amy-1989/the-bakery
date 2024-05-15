9fce28dc97b6b84c8dd94250c3956cbc48f8ef0f
amy-1989 <amy1989moran@gmail.com>
2024-05-15 21:17:12 +0000

    add update address form and profile template

---
+++ profiles/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+from .models import UserProfile, UserAddress
+
+
+class ProfilesForm(forms.ModelForm):
+    class Meta:
+        model = UserAddress
+        fields = ( 'phone_number',
+                  'street_address1', 'street_address2',
+                  'town_or_city', 'postcode', 
+                  'county', 'country', 'is_primary')
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+
+        self.fields['phone_number'].widget.attrs['autofocus'] = True
+        self.fields['country'].widget.attrs['class'] = 'stripe-style-input'
+        self.fields['phone_number'].widget.attrs['placeholder'] = 'Phone Number'
+        self.fields['postcode'].widget.attrs['placeholder'] = 'Postcode'
+        self.fields['town_or_city'].widget.attrs['placeholder'] = 'Town or City'
+        self.fields['street_address1'].widget.attrs['placeholder'] = 'Street Address 1'
+        self.fields['street_address2'].widget.attrs['placeholder'] = 'Street Address 2'
+        self.fields['county'].widget.attrs['placeholder'] = 'County'
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+            self.fields[field].label = ''
+            self.fields['is_primary'].label = 'Tick to set as primary address'
--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import UserProfile, UserAddress
+from .forms import ProfilesForm
+
+
+def profile(request):
+    """ Display the user's profile. """
+    profile = get_object_or_404(UserProfile, user=request.user)
+
+    if request.method == 'POST':
+        form = ProfilesForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated successfully')
+
+    form = ProfilesForm(instance=profile)
+    orders = profile.orders.all()
+
+    template = 'profiles/profile.html'
+    context = {
+        'form': form,
+        'orders': orders,
+        'on_profile_page': True
+    }
+
+    return render(request, template, context)
