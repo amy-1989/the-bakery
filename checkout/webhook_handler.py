63be4f55cd70001e57d8810f5de84809b95ba59e
amy-1989 <amy1989moran@gmail.com>
2024-05-29 19:35:29 +0000

    update webhook handler to handle profiles and save address info

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 
 from .models import Order, OrderLineItem
 from products.models import Product
+from profiles.models import UserProfile, UserAddress
 
 import json
 import time
@@ -43,6 +44,24 @@ class StripeWH_Handler:
             if value == "":
                 shipping_details.address[field] = None
 
+         # Update profile information if save_info was checked
+        profile = None
+        address = None
+        username = intent.metadata.username
+        if username != 'AnonymousUser':
+            profile = UserProfile.objects.get(user__username=username)
+            address = UserAddress.objects.filter(profile=profile, is_primary=True).first()
+            if save_info:
+                address.phone_number = shipping_details.phone
+                address.country = shipping_details.address.country
+                address.postcode = shipping_details.address.postal_code
+                address.town_or_city = shipping_details.address.city
+                address.street_address1 = shipping_details.address.line1
+                address.street_address2 = shipping_details.address.line2
+                address.county = shipping_details.address.state
+                profile.save()
+                address.save()
+
         order_exists = False
         attempt = 1
         while attempt <= 5:
@@ -75,6 +94,7 @@ class StripeWH_Handler:
             try:
                 order = Order.objects.create(
                     full_name=shipping_details.name,
+                    user_profile=profile,
                     email=billing_details.email,
                     phone_number=shipping_details.phone,
                     country=shipping_details.address.country,
